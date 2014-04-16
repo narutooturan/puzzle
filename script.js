568dfcd40f697adfc8d39b95a3d339d4e80007ae
GoneWithSin <liubingyao@gmail.com>
2014-04-16 19:56:41 +0800

    实现generateMoves

--- script.js
+++ script.js
@@ -38,24 +38,102 @@ function iterativeDeepening ( state ) {
 iterative deepening: run search function with specific depth(threshold) iteratively.
 state: puzzle's state, [0,1,2,3,4,5,6,7,8]
 */
+
 	var threshold = manhattan( state );
 　　var solution = false;
 　　while ( !solution ) {
-		if ( search( state, 0, threshold ) ) {
+		// if find solution, break while
+		if ( solution = search( state, 0, threshold ) ) {
 			break;
 		}
-        threshold = incrementThreshold();
+		// deepen search depth
+        threshold = incrementThreshold( threshold );
 	}
+	// show find solution
 　　console.log("Solution found at depth: " + threshold);
-　　return threshold;
+	return true;
 }
 
 function search ( state, g, threshold ) {
-	return threshold;
+	h = manhattan( state );
+　　if ( h == 0 ) {
+		return true;
+	}
+　　　    
+　　if ( g + h > threshold ) {
+		return false;
+	}
+
+　　movelist = generateMoves( state );
+　　/*
+	Foreach move on movelist do
+　　　   MakeMove( state, move )
+　　　   If Search( state, g+1, threshold ) == TRUE then
+　　　       Return TRUE
+　　　   undoMove( state, move )
+　　Endfor
+	*/
+	return false;
 }
 
-function incrementThreshold () {
-	return 0;
+function generateMoves ( state ) {
+// generate moves, clockwise
+// movelisht: [([1,1],up),([1,1],up)]
+// move: ([1,1],up)
+// move: ([1,1],right)
+// move: ([1,1],down)
+// move: ([1,1],left)
+
+　　// movelist
+	var movelist = [];
+	// empty tile's neighbours' position
+	var neighbours = [];
+	// search empty tile
+	for ( s in state ) {
+		if ( state[s] == 0 ){
+			// get empty tile's position
+			var sPosition = [ Math.floor(s/3), (s%3) ];
+			// get empty tile's neighbours' position
+			// up
+			neighbours.push([ sPosition[0] - 1, sPosition[1] ]);
+			// right
+			neighbours.push([ sPosition[0], sPosition[1] + 1 ]);
+			// down
+			neighbours.push([ sPosition[0] + 1, sPosition[1] ]);
+			// left
+			neighbours.push([ sPosition[0], sPosition[1] - 1 ]);
+			break;
+		}
+	}
+	// check if neighbour is available ==> if array's index is not out of bound
+	for(n in neighbours){
+		n = parseInt(n);
+		if( ( neighbours[n][0] >= 0 ) && ( neighbours[n][0] <= 3 ) && ( neighbours[n][1] >= 0 ) && ( neighbours[n][1] <= 3 ) ){
+			// add neighbour's index and direction to movelist
+			switch(n){
+				case 0:
+					movelist.push([neighbours[n], "down"]);
+				break;
+				case 1:
+					movelist.push([neighbours[n], "left"]);
+				break;
+				case 2:
+					movelist.push([neighbours[n], "up"]);
+				break;
+				case 3:
+					movelist.push([neighbours[n], "right"]);
+				break;
+			}
+		}
+	}
+
+	return movelist;
+}
+
+function incrementThreshold ( threshold ) {
+// deepen search depth
+	//?
+	return threshold + 2;
 }
 
 function manhattan ( state ){
@@ -75,12 +153,17 @@ function manhattan ( state ){
 	e.g. D = 3 + 4;
 
 	state: puzzle's state, [0,1,2,3,4,5,6,7,8]
-	manhattanCost: total manhattan distance in puzzle
+	manhattanCost: total manhattan distance in puzzle. PS: 0 is not included
 */
+
+	// total manhattan distance in puzzle
 	var manhattanCost = 0;
+	// right puzzle's order
 	var initPuzzle = [0,1,2,3,4,5,6,7,8];
+	// current puzzle's order
 	var currentPuzzle = state;
 
+	// calculate manhattanCost
 	for ( i in initPuzzle){
 		for ( j in currentPuzzle){
 			if ( ( initPuzzle[i] == currentPuzzle[j] ) && ( initPuzzle[i]) != 0 ){
