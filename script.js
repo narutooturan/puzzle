37b831a622e8e7f9cb72801d39594104af341361
GoneWithSin <liubingyao@gmail.com>
2014-04-26 14:57:21 +0800

    1.整理代码
    2.整理注释

--- script.js
+++ script.js
@@ -1,284 +1,7 @@
-/*
-puzzle AI:
-1. IDA*
-2. Manhattan
-
-Integer <- IterativeDeepening( state )
-　　　Threshold <- ManhattanEvaluate( state )
-　　　Solution <- NO
-　　　While solution == NO do
-　　　    If Search( state, 0, threshold ) == YES then
-　　　        Break
-        Threshold <- IncrementThreshold()
-　　　Endwhile
-　　　Output <- “Solution found at depth” threshold
-　　　Return threshold
-end
-
-Boolean <- Search( state, g, threshold )
-　　　H <- ManhattanEvaluate( state )
-　　　If h == 0 then
-　　　    Return TRUE
-　　　If g+h > threshold then
-　　　    Return FALSE
-　　　Movelist <- GenerateMoves( state )
-　　　Foreach move on movelist do
-　　　    MakeMove( state, move )
-　　　    If Search( state, g+1, threshold ) == TRUE then
-　　　        Return TRUE
-　　　    undoMove( state, move )
-　　　Endfor
-　　　Return FALSE
-end
-
-
-
-test state:
-[1,2,0,3,6,8,7,5,4]
-[1,8,4,5,0,7,3,2,6]
-
-*/
-
-
-
-
-// current puzzle, default puzzle
-var curPuzzle = [1,2,0,3,6,8,7,5,4];
-// this game's puzzle
-var thisPuzzle = curPuzzle.concat();
-
-// clear after solve!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// last move's direction
-var lastMoveDir = "";
-// last move
-var lastMove = [];
-
-function iterativeDeepening () {
-/*
-iterative deepening: run search function with specific depth(threshold) iteratively.
-state: puzzle's state, [0,1,2,3,4,5,6,7,8]
-*/
-	var state = [];
-	state = curPuzzle.concat();
-	thisPuzzle = curPuzzle.concat();
-	lastMoveDir = "";
-	lastMove = [];
-	console.log("state: " + curPuzzle);
-	
-	var threshold = manhattan( state );
-　　var solution = false;
-　　while ( !solution ) {
-		// if find solution, break while
-		if ( solution = search( state, 0, threshold ) ) {
-			break;
-		}
-		// deepen search depth
-        threshold = incrementThreshold( threshold );
-	}
-	
-	console.log("solution: " + lastMove);
-	showSolution();
-	// show find solution
-	return true;
-
-}
-
-function search ( state, g, threshold ) {
-	var h = manhattan( state );
-	if ( h == 0 ) {
-		return true;
-	}
-　　　    
-　　if ( g + h > threshold ) {
-		return false;
-	}
-
-　　var movelist = generateMoves( state );
-
-	for( move in movelist ) {
-		move = movelist[move];
-		state = makeMove( state, move );
-		lastMove.push(move);
-		lastMoveDir = move[1];
-		if ( search( state, g + 1, threshold ) == true){
-			return true;
-		}
-		state = undoMove( state, lastMove[lastMove.length-1] );
-		lastMove.pop();
-	}
-
-	return false;
-}
-
-function makeMove ( state, move ) {
-	// moving node's direction and array index
-	var moveDir = move[1];
-	var moveNode = move[0];
-	// node's array index after move
-	var node = [];
-	switch ( moveDir ) {
-		case "up":
-			node[0] = moveNode[0] - 1;
-			node[1] = moveNode[1];
-		break;
-		case "left":
-			node[1] = moveNode[1] - 1;
-			node[0] = moveNode[0];
-		break;
-		case "down":
-			node[0] = moveNode[0] + 1;
-			node[1] = moveNode[1];
-		break;
-		case "right":
-			node[1] = moveNode[1] + 1;
-			node[0] = moveNode[0];
-		break;
-	}
-	// change state
-	state[ node[0]*3 + node[1] ] = state[ moveNode[0]*3 + moveNode[1] ];
-	state[ moveNode[0]*3 + moveNode[1] ] = 0;
-	return state;
-}
-
-function undoMove ( state, move ) {
-	// moving node's direction and array index
-	var moveDir = move[1];
-	var moveNode = move[0];
-	// node's array index after move
-	var node = [];
-	switch ( moveDir ) {
-		case "up":
-			node[0] = moveNode[0] - 1;
-			node[1] = moveNode[1];
-		break;
-		case "left":
-			node[1] = moveNode[1] - 1;
-			node[0] = moveNode[0];
-		break;
-		case "down":
-			node[0] = moveNode[0] + 1;
-			node[1] = moveNode[1];
-		break;
-		case "right":
-			node[1] = moveNode[1] + 1;
-			node[0] = moveNode[0];
-		break;
-	}
-	state[ moveNode[0]*3 + moveNode[1] ] = state[ node[0]*3 + node[1] ];
-	state[ node[0]*3 + node[1] ] = 0;
-	return state;
-}
-
-function generateMoves ( state ) {
-// generate moves, clockwise
-// movelisht: [([1,1],up),([1,1],up)]
-// move: ([1,1],up)
-// move: ([1,1],right)
-// move: ([1,1],down)
-// move: ([1,1],left)
-
-　　// movelist
-	var movelist = [];
-	// empty tile's neighbours' position
-	var neighbours = [];
-	// search empty tile
-	for ( s in state ) {
-		if ( state[s] == 0 ){
-			// get empty tile's position
-			var sPosition = [ Math.floor(s/3), (s%3) ];
-			// get empty tile's neighbours' position
-			// up
-			neighbours.push([ sPosition[0] - 1, sPosition[1] ]);
-			// right
-			neighbours.push([ sPosition[0], sPosition[1] + 1 ]);
-			// down
-			neighbours.push([ sPosition[0] + 1, sPosition[1] ]);
-			// left
-			neighbours.push([ sPosition[0], sPosition[1] - 1 ]);
-			break;
-		}
-	}
-	// check if neighbour is available ==> if array's index is not out of bound
-	for(n in neighbours){
-		n = parseInt(n);
-		if( ( neighbours[n][0] >= 0 ) && ( neighbours[n][0] < 3 ) && ( neighbours[n][1] >= 0 ) && ( neighbours[n][1] < 3 ) ){
-			// add neighbour's index and direction to movelist
-			switch(n){
-				case 0:
-					if ( lastMoveDir != "up" ){
-						movelist.push([neighbours[n], "down"]);
-					}
-				break;
-				case 1:
-					if ( lastMoveDir != "right" ){
-					movelist.push([neighbours[n], "left"]);
-					}
-				break;
-				case 2:
-					if ( lastMoveDir != "down" ){
-					movelist.push([neighbours[n], "up"]);
-					}
-				break;
-				case 3:
-					if ( lastMoveDir != "left" ){
-					movelist.push([neighbours[n], "right"]);
-					}
-				break;
-			}
-		}
-	}
-
-	return movelist;
-}
-
-function incrementThreshold ( threshold ) {
-// deepen search depth
-	//?
-	return threshold + 2;
-}
-
-function manhattan ( state ){
-/* 	Manhattan distance
-	e.g.
-				↓ end point
-	__________
-	|__|__|__|		|←L(y)
-	|__|__|__|		|
-	|__|__|__|		|
-	|__|__|__|		|
-	↑ begin point
-	 _________ 		←L(x)
-
-	distance = L(x)+L(y);
-	Manhattan distance is sum of horizontal distance and vertical distance.
-	e.g. D = 3 + 4;
-
-	state: puzzle's state, [0,1,2,3,4,5,6,7,8]
-	manhattanCost: total manhattan distance in puzzle. PS: 0 is not included
-*/
-
-	// total manhattan distance in puzzle
-	var manhattanCost = 0;
-	// right puzzle's order
-	var initPuzzle = [0,1,2,3,4,5,6,7,8];
-	// current puzzle's order
-	var currentPuzzle = state;
-
-	// calculate manhattanCost
-	for ( i in initPuzzle){
-		for ( j in currentPuzzle){
-			if ( ( initPuzzle[i] == currentPuzzle[j] ) && ( initPuzzle[i]) != 0 ){
-				var rowCost = Math.abs( Math.floor(i/3) - Math.floor(j/3) );
-				var columnCost = Math.abs( Math.floor(i%3) - Math.floor(j%3) );
-				manhattanCost += rowCost + columnCost;
-			}
-		}
-	}
-
-	return manhattanCost;
-	
-}
-
+/****** *************** ******/
+/****** button function ******/
+/****** *************** ******/
+// new puzzle button
 function newPuzzle() {
 // generate puzzle state randomly
 	// state array
@@ -495,5 +218,124 @@ function newPuzzle() {
 	curPuzzle = puzzle;
 	thisPuzzle = curPuzzle.concat();
 	// show state in UI
-	showPuzzle();
+	showSolutionTips();
+}
+
+// solve button
+// run iterativeDeepening(), then show solution in information panel
+// show solution
+function showSolution(){
+	var solution = "";
+	for(move in lastMove){
+		solution += ">>> " + lastMove[move][1] + " ";
+	}
+	document.getElementById("solution").innerText = solution;
+}
+
+// reset button
+// reset puzzle array, then show puzzle
+function reset() {
+	curPuzzle = thisPuzzle.concat();
+	showSolutionTips();
+}
+
+// show infomation
+// show solution tips
+function showSolutionTips(){
+	document.getElementById("solution").innerText = "Hit [SOLVE]! Show solution!";
+	// show picture in UI depending on current state
+	showPic();
+}
+
+// show puzzle
+// show puzzle state in UI
+function showPic(){
+	for ( t in curPuzzle){
+		$(".tile")[t].innerHTML = "<p style=\"background-image: url(image/"+curPuzzle[t]+".jpg); \">" + curPuzzle[t] + "</p>";
+		if (curPuzzle[t]=="0"){
+			$(".tile")[t].innerHTML= "<p style=\"background-color:#fff\" > </p>";
+		}
+	}
+}
+
+
+
+
+/****** ********** ******/
+/****** game event ******/
+/****** ********** ******/
+// key listener
+$(document).keydown(function(){
+	var dir = "";
+	lastMoveDir = "";
+	console.log(event.keyCode);
+	switch ( event.keyCode ){
+		case 65:
+		// left
+			dir = "left";
+			var moves = generateMoves(curPuzzle);
+			for ( m in moves ){
+				m = moves[m];
+				if ( m[1] == dir ){
+					console.log(dir);
+					makeMove( curPuzzle, m );
+					showPic();
+					break;
+				}
+			}
+		break;
+		case 87:
+		// up
+			dir = "up";
+			var moves = generateMoves(curPuzzle);
+			for ( m in moves ){
+				m = moves[m];
+				if ( m[1] == dir ){
+					console.log(dir);
+					makeMove( curPuzzle, m );
+					showPic();
+					break;
+				}
+			}
+		break;
+		case 68:
+		// right
+			dir = "right";
+			var moves = generateMoves(curPuzzle);
+			for ( m in moves ){
+				m = moves[m];
+				if ( m[1] == dir ){
+					console.log(dir);
+					makeMove( curPuzzle, m );
+					showPic();
+					break;
+				}
+			}
+		break;
+		case 83:
+		// down
+			dir = "down";
+			var moves = generateMoves(curPuzzle);
+			for ( m in moves ){
+				m = moves[m];
+				if ( m[1] == dir ){
+					console.log(dir);
+					makeMove( curPuzzle, m );
+					showPic();
+					break;
+				}
+			}
+		break;
+	}
+});
+		
+
+/****** ********* ******/
+/****** init game ******/
+/****** ********* ******/
+function init() {
+	// generate puzzle randomly
+	newPuzzle();
+	// show puzzle
+	showPic();
 }
