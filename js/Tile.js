aee1dbfac30808f7f4bfde8ab40dc5dc82737f7c
GoneWithSin <liubingyao@gmail.com>
2014-05-03 15:03:44 +0800

    修改界面

--- js/Tile.js
+++ js/Tile.js
@@ -11,47 +11,47 @@ function tile(index) {
 	// image
 	switch(this.index) {
 		case 0:
-			this.image.src = 'image/0.jpg';
+			this.image.src = 'image/0.png';
 			this.x = 0;
 			this.y = 0;
 		break;
 		case 1:
-			this.image.src = 'image/1.jpg';
+			this.image.src = 'image/1.png';
 			this.x = this.width;
 			this.y = 0;
 		break;
 		case 2:
-			this.image.src = 'image/2.jpg';
+			this.image.src = 'image/2.png';
 			this.x = this.width * 2;
 			this.y = 0;
 		break;
 		case 3:
-			this.image.src = 'image/3.jpg';
+			this.image.src = 'image/3.png';
 			this.x = 0;
 			this.y = this.width;
 		break;
 		case 4:
-			this.image.src = 'image/4.jpg';
+			this.image.src = 'image/4.png';
 			this.x = this.width;
 			this.y = this.width;
 		break;
 		case 5:
-			this.image.src = 'image/5.jpg';
+			this.image.src = 'image/5.png';
 			this.x = this.width * 2;
 			this.y = this.width;
 		break;
 		case 6:
-			this.image.src = 'image/6.jpg';
+			this.image.src = 'image/6.png';
 			this.x = 0;
 			this.y = this.width * 2;
 		break;
 		case 7:
-			this.image.src = 'image/7.jpg';
+			this.image.src = 'image/7.png';
 			this.x = this.width;
 			this.y = this.width * 2;
 		break;
 		case 8:
-			this.image.src = 'image/8.jpg';
+			this.image.src = 'image/8.png';
 			this.x = this.width * 2;
 			this.y = this.width * 2;
 		break;
